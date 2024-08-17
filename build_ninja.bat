acfe588ed48350d87547165ffdd363eada1541b1
Sam <56632175+SamsRepos@users.noreply.github.com>
2024-08-17 14:48:48 +0100

    Renaming SoundCrux => SoundCrux_RtAudio

--- build_ninja.bat
+++ build_ninja.bat
@@ -1,12 +1,16 @@
 @echo off
+
 rem Delete the build directory if it exists
 if exist build rmdir /S /Q build
 
-rem Run CMake to configure the project and generate build files
-cmake -S . -B build -G Ninja -DCMAKE_TOOLCHAIN_FILE="C:/vcpkg/scripts/buildsystems/vcpkg.cmake"
+rem Run vcpkg install with the correct triplet
+vcpkg install --triplet x64-windows
+
+rem Run CMake to configure the project and generate build files with Ninja
+cmake -S . -B build -G "Ninja" -DCMAKE_TOOLCHAIN_FILE="C:/vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET="x64-windows"
 
 rem Build the project
-cmake --build build
+cmake --build build --verbose
 
 rem Pause the command prompt to see output (optional)
 pause
